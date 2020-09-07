4be9bf49b8104975c3ad19a72d22d29bea159643
MrZQAQ <1779729350@qq.com>
2020-09-07 21:34:23 +0800

    <GUI> 整体重构，面向对象！

--- export_video_GUI.py
+++ export_video_GUI.py
@@ -18,17 +18,7 @@ class convert_thread (threading.Thread):   #继承父类threading.Thread
     def run(self):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数 
         create_video(self.codecfun, self.video_bitrate, self.audio_bitrate)
 
-#转换开始标志位
-convert_started = 0
-convert = convert_thread('libx264', '2000', '128')
-
-#主窗口
-window = tk.Tk()
-window.title("Export Video from Blibili")
-window.geometry("350x110")
-
-#过滤用户输入
-def isnot_qualified(s):
+def isnot_qualified(s):    #过滤用户输入
     try:
         if int(s)>0 :
             return False
@@ -37,76 +27,104 @@ def isnot_qualified(s):
     except ValueError:
         return True
 
-#视频码率输入
-vlable = tk.Label(window,text='视频码率：')
-vbit = tk.Entry(window, width=10)
-vscale = tk.Label(window,text='k')
-vlable.grid(row=1,column=0)
-vbit.grid(row=1,column=1)
-vscale.grid(row=1,column=2)
 
-#音频码率输入
-alable = tk.Label(window,text='音频码率：')
-abit = tk.Entry(window,width=10)
-ascale = tk.Label(window,text='k')
-alable.grid(row=2,column=0)
-abit.grid(row=2,column=1)
-ascale.grid(row=2,column=2)
+class bit_input(tk.Frame):          #封装码率输入fream
+    def __init__(self, bit_name, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets(bit_name)
 
-#编码器选择
-clable = tk.Label(window,text='编码器：')
-clable.grid(row=3,column=0)
-codecombo = ttk.Combobox(window,state='readonly')
-codecombo['values'] = ('libx264','h264_qsv','h264_nvenc','h264_amf')
-codecombo.current(0)
-codecombo.grid(row=3,column=1)
-
-#转换按钮动作
-def start_convert():
-    video_bitrate = vbit.get()
-    audio_bitrate = abit.get()
-    codecfun = codecombo.get()
-    if isnot_qualified(video_bitrate) :
-        vbit.select_clear()
-        messagebox.showinfo("Export Video from Blibili", "视频码率错误！")
-    elif isnot_qualified(audio_bitrate) :
-        abit.select_clear()
-        messagebox.showinfo("Export Video from Blibili", "音频码率错误！")
-    else:
-        global convert_started
-        if convert_started == 1 :
-            messagebox.showinfo('Export Video from Blibili','请勿重复点击')
-        else:
-            convert.codecfun = codecfun
-            convert.video_bitrate = video_bitrate
-            convert.audio_bitrate = audio_bitrate
-            convert.start()
-            convert_started = 1
-
-
-#转换按钮
-btn = tk.Button(window,text='开始转换',command=start_convert)
-btn.grid(row=2,column=4)
-
-#帮助按钮动作
-def start_help():
-    webbrowser.open("https://github.com/MrZQAQ/bilibili_recoder")
+    def create_widgets(self, bit_name):
+        self.bit_lable = tk.Label(self)
+        self.bit_lable["text"] = bit_name
+        self.bit_entry = tk.Entry(self)
+        self.bit_entry["width"] = "10"
+        self.bit_scale = tk.Label(self)
+        self.bit_scale["text"] = "k"
+        self.bit_lable.pack(side=tk.LEFT)
+        self.bit_entry.pack(side=tk.LEFT)
+        self.bit_scale.pack(side=tk.LEFT)
     
-#帮助按钮
-helpbtn = tk.Button(window,text='点此获取帮助',command=start_help)
-helpbtn.grid(row=5,column=4)
+    def getvar(self):
+        return self.bit_entry.get()
 
-#检测转换是否完成
-def convert_done():
-    global convert_started
-    if convert_started == 1:
-        if convert.isAlive() == False :
-            convert_started = 0
-            #convert = convert_thread('libx264', '2000', '128')
-            if messagebox.showinfo('Export Video from Blibili','转换完成！') == 'ok' :
-                exit(0)
-    window.after(500,convert_done)
+class coded_selector(tk.Frame):         #封装编码器选择fream
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.codec_lable = tk.Label(self)
+        self.codec_lable["text"] = "编码器 : "
+        self.codec_combo = ttk.Combobox(self)
+        self.codec_combo["state"] = "readonly"
+        self.codec_combo["values"] = ('libx264','h264_qsv','h264_nvenc','h264_amf')
+        self.codec_combo["width"] = 10
+        self.codec_combo.current(0)
+        self.codec_lable.pack(side=tk.LEFT)
+        self.codec_combo.pack(side=tk.LEFT)
+
+    def getvar(self):
+        return self.codec_combo.get()
+
+
+class Application(tk.Frame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.convert_started = 0
+        self.convert = convert_thread('libx264', '2000', '128')
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):          #创建容器内部各个子组件
+        self.input_fream = tk.Frame(self)       #输入子组件
+        self.input_fream.pack(side=tk.LEFT)
+        self.input_fream.vbitlable = bit_input("视频码率 : ", self.input_fream)      #最上面是视频码率输入
+        self.input_fream.abitlable = bit_input("音频码率 : ", self.input_fream)      #音频码率输入
+        self.input_fream.codec = coded_selector(self.input_fream)                  #编码器选择
+
+        self.start_btn = tk.Button(self, text="开始转换", command=self.start_convert)       #开始按钮
+        self.start_btn.pack(side=tk.LEFT)
+        self.help_btn = tk.Button(self, text="帮助", command=self.start_help)           #帮助按钮
+        self.help_btn.pack(side=tk.LEFT)
+
+    def start_convert(self):        #开始按钮动作
+        if isnot_qualified(self.input_fream.vbitlable.getvar()) :
+            messagebox.showinfo("Export Video from Blibili", "视频码率错误！")
+        elif isnot_qualified(self.input_fream.abitlable.getvar()) :
+            messagebox.showinfo("Export Video from Blibili", "音频码率错误！")
+        else:
+            if self.convert_started == 1 :
+                messagebox.showinfo('Export Video from Blibili','正在转换\n请勿重复点击')
+            else:
+                self.convert.codecfun = self.input_fream.codec.getvar()
+                self.convert.video_bitrate = self.input_fream.vbitlable.getvar()
+                self.convert.audio_bitrate = self.input_fream.abitlable.getvar()
+                self.convert.start()        #启动转换线程
+                self.convert_started = 1        #线程启动标志
+                self.after(500,self.convert_done)       #启动线程结束监听
+
+    def start_help(self): 
+        webbrowser.open("https://github.com/MrZQAQ/bilibili_recoder")
+        
+    def convert_done(self):     #检测转换是否完成
+        if self.convert_started == 1:
+            if self.convert.is_alive() == False :
+                self.convert_started = 0
+                if messagebox.showinfo('Export Video from Blibili','转换完成！') == 'ok' :
+                    exit(0)
+        self.after(500,self.convert_done)
+
 
 if __name__ == '__main__':
-    window.after(500,convert_done)
-    window.mainloop()
+    root = tk.Tk()
+    root.title("Export Video from Blibili")
+    root.geometry("350x110")
+    app = Application(master=root)
+    app.mainloop()
+
